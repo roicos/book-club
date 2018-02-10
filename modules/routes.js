4582c10d202fd4c37b7fabf78c97122a70ea81d7
Ekaterina Dontsova <ekaterina.dontsova@gmail.com>
2018-02-09 17:04:34 -0800

    edit profile

--- modules/routes.js
+++ modules/routes.js
@@ -75,7 +75,7 @@ module.exports = function (express, app, path, bcrypt, dbClient) {
 			if (err){
 				handleError("Error to get states list: " + err);
 			} else {
-				res.render("registration", {"message" : "", "states" : result.rows});
+				res.render("registration", {"message" : "", "states" : result.rows, "data" : {"username" : "", "city" : "", "state" : 0}});
 			}
 		});
     });
@@ -110,6 +110,71 @@ module.exports = function (express, app, path, bcrypt, dbClient) {
 							});
 						} else {
 							res.status(400).send({"message" : "Passwords are not the same"});
+						}
+					}
+				}
+			});
+		} else {
+			res.status(400).send({"message" : "Username and city should not be empty."});
+		}
+	});
+
+	app.get("/profile", checkAuth, function (req, res, next) {
+		var userId = req.session.user;
+        dbClient.query("select * from users where id = " + userId, (errUsers, resultUsers) => {
+			if (errUsers){
+				handleError("Error to get user: " + errUser);
+			} else {
+				dbClient.query("select * from states", (errStates, resultStates) => {
+					if (errStates){
+						handleError("Error to get states list: " + errStates);
+					} else {
+						res.render("registration", {"message" : "", "states" : resultStates.rows, "data" : {"username" : resultUsers.rows[0].username, "city" : resultUsers.rows[0].city, "state" : resultUsers.rows[0].stateid}});
+					}
+				});
+			}
+		});
+    });
+
+    app.post("/editprofile", function (req, res, next) {
+    
+		var userid = req.body.userid.trim();
+		var username = req.body.username.trim();
+		var password1 = req.body.password1.trim();
+		var password2 = req.body.password2.trim();
+		var city = req.body.city.trim();
+		var state = req.body.state.trim();
+
+		if(username !="" && city !=""){
+			dbClient.query("select id from users where username = '" + username + "'", (errSelect, resultSelect) => {
+				if (errSelect){
+					handleError("Error to check if user exists: " + errSelect);
+				} else {
+					if(resultSelect.rows.length > 1){
+						res.status(400).send({"message" : "This username is already exists."});
+					} else {
+						if(password1 != "******"){
+							if(password1 == password2){
+								bcrypt.hash(password1, 10, function(err, passHash) {
+									dbClient.query("update users set username = '" + username + "', password = '"+ passHash +"', city = '" + city + "', stateid = " + state +" where id = " + userid, (errUpdate, resultUpdate) => {
+										if (errUpdate){
+											handleError("Error to update user: " + errUpdate);
+										} else {
+											res.status(200).send({"message" : "profile updated successfully: ", "redirect" : "/profile"});
+										}
+									});
+								});
+							} else {
+								res.status(400).send({"message" : "Passwords are not the same"});
+							}
+						} else {
+							dbClient.query("update users set username = '" + username + "', city = '" + city + "', stateid = " + state +" where id = " + userid, (errUpdate, resultUpdate) => {
+								if (errUpdate){
+									handleError("Error to update user: " + errUpdate);
+								} else {
+									res.status(200).send({"message" : "profile updated successfully: ", "redirect" : "/profile"});
+								}
+							});
 						}
 					}
 				}
